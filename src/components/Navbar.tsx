d9930aa653388e0cd83d6128373552733c09a57c
sumionochi <aaditya.srivastava.connect@gmail.com>
2023-11-26 06:33:56 +0530

    styled the active link in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,24 @@
+'use client'
 import React from 'react'
 import Logo from './Logo'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 type Props = {}
 
 const Navbar = (props: Props) => {
-  const links = [{label:'Dashboard', href:'/'},{label:'Issues', href:'/issues'}]
+  const links = [{label:'Dashboard', href:'/'},{label:'Issues', href:'/Issues'}]
+  
+  /*to highlight the opened page*/
+  const currPath = usePathname();
+  
   return (
     <header className='sticky top-0 z-50 backdrop-blur-sm mx-auto'>
       <nav className='flex max-w-7xl gap-2 flex-col sm:flex-row items-center p-5 pl-2 bg-none mx-auto'>
         <Logo/>
         <div className='flex-1 flex items-center justify-end space-x-4' >
-          <div className='rounded-3xl gap-2 flex-row sm:flex-row sm:gap-4 px-8 py-4 bg-black hover:bg-black flex justify-center items-center text-sm text-white outline outline-2 outline-white/30 hover:outline-white/30'>
-            {links.map(link=><Link key={link.href} className='' href={link.href}>{link.label}</Link>)}
+          <div className={'rounded-3xl gap-2 flex-row sm:flex-row sm:gap-4 px-8 py-4 bg-black hover:bg-black flex justify-center items-center text-sm text-white outline outline-2 outline-white/30 hover:outline-white/30'}>
+            {links.map(link=><Link key={link.href} className={`${link.href === currPath ? 'font-semibold' : 'font-base'} hover:text-white/50 transition-all`} href={link.href}>{link.label}</Link>)}
           </div>
         </div>
       </nav>
