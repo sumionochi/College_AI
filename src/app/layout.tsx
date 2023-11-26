b9a4b9e22ba0f218424935ff0312d6b4d71a78d8
sumionochi <aaditya.srivastava.connect@gmail.com>
2023-11-26 06:18:01 +0530

    build the navbar and home ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Lexend } from 'next/font/google'
 import './globals.css'
+import { cn } from '@/lib/utils'
+import Navbar from '@/components/Navbar'
 
-const inter = Inter({ subsets: ['latin'] })
+const lexend = Lexend({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +17,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning={true}>
+      <body className={cn(lexend.className, 'antialiased min-h-screen border-none outline-none', 'scrollbar scrollbar-thumb scrollbar-thumb-white scrollbar-track-slate-700 bg-gradient-to-br from-rose-400 to-orange-300 dark:bg-gradient-to-br dark:from-gray-900 dark:via-purple-900 dark:to-violet-600')} suppressHydrationWarning={true}>
+        <Navbar/>
+        {children}
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,90 @@
+import { Button } from '@/components/ui/button'
+import { ArrowRight, ArrowUpRight, BookText, BotIcon, Github, Key, Linkedin, Lock, Mail, ScrollText, Text } from 'lucide-react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
+  const session = true;
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='flex relative overflow-hidden antialiased min-h-screen flex-col items-center justify-between pt-0'>
+        <div className='flex flex-col items-center min-h-screen pt-10 sm:pt-28'>
+          <div className='flex text-white p-10 pb-0 mb-10 rounded-md gap-8 flex-col max-w-5xl'>
+            <h1 className='text-center font-semibold text-3xl md:text-5xl xl:text-6xl'>
+              Remote Health Consultation from Anywhere!
+            </h1>
+            <h2 className='text-center text-xl'>              
+              Access timely medical assistance seamlessly. Get right level of <span className='text-pink-900 dark:text-purple-300'>medical expertise with the insights of AI.</span>
+            </h2>
+            
+          </div>
+          <div className='pt-10 grid mx-auto text-white max-w-7xl gap-4 place-items-center px-10 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-3 grid-cols-1'>
+          {/* <Button className='p-6 shadow-md shadow-black bg-gradient-to-r from-rose-500 to-blue-500 border text-white rounded-md border-none'>
+            <Book className='mr-2'/>
+            AI Generated Courses
+          </Button> */}
+          {/* <Button className='p-6 shadow-md shadow-black bg-gradient-to-r from-teal-500 to-black border text-white rounded-md border-none'>
+            <ScrollText className='mr-2'/>
+            Adaptive Quizzes
+          </Button> */}
+          <Button className='rounded-3xl bg-black hover:bg-black flex flex-row justify-center items-center pb-2 text-sm text-white outline outline-2 outline-white/30 hover:outline-white/30'>
+            <Text className='mr-2 w-5'/>
+            Multi Lingual Translation
+          </Button>
+          <Button className='rounded-3xl bg-black hover:bg-black flex flex-row justify-center items-center pb-2 text-sm text-white outline outline-2 outline-white/30 hover:outline-white/30'>
+            <Lock className='mr-2 w-5'/>
+            Secure Chatroom Authentication
+          </Button>
+          <Button className='rounded-3xl bg-black hover:bg-black flex flex-row justify-center items-center pb-2 text-sm text-white outline outline-2 outline-white/30 hover:outline-white/30'>
+            <Key className='mr-2 w-5'/>
+            User Access Levels
+          </Button>
+          <Button className='rounded-3xl bg-black hover:bg-black flex flex-row justify-center items-center pb-2 text-sm text-white outline outline-2 outline-white/30 hover:outline-white/30'>
+            <ScrollText className='mr-2 w-5'/>
+            AI Summarization
+          </Button>
+          <Button className='rounded-3xl bg-black hover:bg-black flex flex-row justify-center items-center pb-2 text-sm text-white outline outline-2 outline-white/30 hover:outline-white/30'>
+            <BookText className='mr-2 w-5'/>
+            Smart Diary
+          </Button>
+          <Button className='rounded-3xl bg-black hover:bg-black flex flex-row justify-center items-center pb-2 text-sm text-white outline outline-2 outline-white/30 hover:outline-white/30'>
+            <BotIcon className='mr-2 w-5'/>
+            Virtual Physician with memory
+          </Button>
+          </div>         
+          <div className='flex text-center p-4 pb-28 pt-0 items-center flex-col gap-4'>
+            <h1 className='text-white px-4 text-xl mt-20'>“Love how the app is so secure, personalized and transparent. Made healthcare more accessible.”</h1>
+            <img src="https://avatars.githubusercontent.com/u/89721628?v=4" className='w-14 h-14 rounded-full border-2 border-white' />
+            <Link href={'https://www.linkedin.com/in/aaditya-srivastava-b4564821b/'}>
+            <h2 className='text-white text-md underline underline-offset-2'>Aaditya Srivastava</h2>
+            </Link>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        {/* <div className='flex my-4 flex-col p-8 rounded-xl max-w-7xl mx-auto bg-[#0D2427] text-white'>
+          <div className='flex flex-col gap-4'>
+            <h1 className='text-start font-semibold text-3xl md:text-5xl xl:text-6xl'>The Mini AI Classroom</h1>
+            <img src="https://d112y698adiu2z.cloudfront.net/photos/production/software_photos/002/650/685/datas/original.png" className='rounded-md object-cover' />
+            <div></div> 
+            <h1 className='text-start font-semibold text-3xl md:text-5xl xl:text-6xl'>Future Plans</h1>
+            <img src="https://d112y698adiu2z.cloudfront.net/photos/production/software_photos/002/647/802/datas/original.png" className='rounded-md object-cover' />
+          </div>
+        </div> */}
+        <div className='flex px-4 pt-1 bg-white/20 text-white justify-between gap-4 flex-row items-center text-primary h-14 absolute bottom-0 w-full'>
+        <h2 className='text-white'>© 2023 Context.AI</h2>
+        <div className='flex flex-row gap-4 justify-center items-center'>
+          <Link href={'https://github.com/sumionochi'}>
+            <Github/>
+          </Link>
+          <Link href={'https://www.linkedin.com/in/aaditya-srivastava-b4564821b/'}>
+            <Linkedin/>
+          </Link>
+          <Link href={'mailto:aaditya.srivastava.connect@gmail.com'}>
+            <Mail/>
+          </Link>
+          <Link href={'https://sumionochi.github.io/Portfolio-landing-page/'}>
+            <ArrowUpRight/>
+          </Link>
+        </div>
+        </div>
+    </div>
   )
 }
