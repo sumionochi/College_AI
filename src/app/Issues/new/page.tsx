aada328c96a8595743020cbff04be4b17ad84680
sumionochi <aaditya.srivastava.connect@gmail.com>
2023-11-26 13:05:52 +0530

    complete the form submission

--- src/app/Issues/new/page.tsx
+++ src/app/Issues/new/page.tsx
@@ -5,18 +5,25 @@ import { Textarea } from '@/components/ui/textarea'
 import React from 'react'
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import {useForm, Controller} from 'react-hook-form'
 
-type Props = {}
+interface Form {
+  title: string;
+  description: string;
+}
 
-const NewIssuePage = (props: Props) => {
+const NewIssuePage = () => {
+  const {register, control, handleSubmit} = useForm<Form>();
+
   return (
-    <div className='max-w-2xl p-8 space-y-4'>
-        <Input placeholder='Title'/>
-        <SimpleMDE placeholder='Description' className='bg-secondary rounded-lg'/>
+    <form onSubmit={handleSubmit((data)=>console.log(data))} className='max-w-2xl p-8 space-y-4'>
+        <Input placeholder='Title' {...register('title')}/>
+        <Controller name='description' control={control} render={({field})=><SimpleMDE placeholder='Description' {...field} className='bg-secondary rounded-lg'/>}/>
+        
         <Button className='rounded-full'>
             Submit New Issue
         </Button>
-    </div>
+    </form>
   )
 }
 
