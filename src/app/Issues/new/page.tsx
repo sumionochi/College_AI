2f0884f9bdbddf23b3dd70370c6f669604cc4071
sumionochi <aaditya.srivastava.connect@gmail.com>
2023-11-26 08:23:10 +0530

    markdown editor added

--- src/app/Issues/new/page.tsx
+++ src/app/Issues/new/page.tsx
@@ -3,15 +3,17 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
 import React from 'react'
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 type Props = {}
 
 const NewIssuePage = (props: Props) => {
   return (
-    <div className='max-w-2xl p-8 space-y-2'>
+    <div className='max-w-2xl p-8 space-y-4'>
         <Input placeholder='Title'/>
-        <Textarea placeholder='Description'/>
-        <Button>
+        <SimpleMDE placeholder='Description' className='bg-secondary rounded-lg'/>
+        <Button className='rounded-full'>
             Submit New Issue
         </Button>
     </div>
